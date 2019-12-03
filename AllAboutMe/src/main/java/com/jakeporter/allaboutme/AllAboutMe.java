9088971b4c04a0301e6421a8fc8f96270494e72a
porter3 <45975762+porter3@users.noreply.github.com>
2019-12-03 10:35:24 -0500

    adding project to repo

---
+++ AllAboutMe/src/main/java/com/jakeporter/allaboutme/AllAboutMe.java
@@ -0,0 +1,22 @@
+package com.jakeporter.allaboutme;
+
+/**
+ *
+ * @author jake
+ */
+public class AllAboutMe {
+    public static void main(String[] args){
+        
+        String name = "Jake";
+        String food = "burritos";
+        int pets = 1;
+        boolean eatenGnocchi = false;
+        byte age = 12;
+        
+        System.out.println("I am " + name + ".");
+        System.out.println("I have " + pets + " pet(s).");
+        System.out.println("My favorite food is " + food + ".");
+        System.out.println("It is " + eatenGnocchi + " that I have eaten gnocchi.");
+        System.out.println("And when I was " + age + " years old, I learned to whistle.");
+    }
+}
