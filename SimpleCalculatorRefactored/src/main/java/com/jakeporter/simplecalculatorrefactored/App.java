37845bb898073e7271a352f80e5b04cb82b6ea32
porter3 <45975762+porter3@users.noreply.github.com>
2019-12-10 14:37:43 -0500

    adding project to repo, some easy but unfinished logic

---
+++ SimpleCalculatorRefactored/src/main/java/com/jakeporter/simplecalculatorrefactored/App.java
@@ -0,0 +1,11 @@
+package com.jakeporter.simplecalculatorrefactored;
+
+/**
+ *
+ * @author jake
+ */
+public class App {
+
+    
+    // how should I go about including the interface in this package?
+}
