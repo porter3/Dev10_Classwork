ad382b4b2a56364cc7b00d1f6b444f47b29e8151
porter3 <45975762+porter3@users.noreply.github.com>
2019-12-18 14:12:28 -0500

    added StringTimes class/tests to project, all pass

---
+++ SectionThreeUnitTests/src/main/java/com/jakeporter/sectionthreeunittests/SayHi.java
@@ -0,0 +1,16 @@
+package com.jakeporter.sectionthreeunittests;
+
+/**
+ *
+ * @author jake
+ */
+public class SayHi {
+
+    // Given a string name (e.g. "Bob"), return a greeting of the form "Hello Bob!"
+    
+    // sayHi("Bob") -> "Hello Bob!"
+    
+    public String sayHi(String name){
+        return "Hello " + name + "!";
+    } 
+}
