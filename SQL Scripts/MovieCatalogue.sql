7bec9b90a4184dca78112f1a0e51a2a6edbaca4d
porter3 <45975762+porter3@users.noreply.github.com>
2020-01-08 16:39:15 -0500

    completed DML exercise portion of this script

--- SQL Scripts/MovieCatalogue.sql
+++ SQL Scripts/MovieCatalogue.sql
@@ -54,9 +54,44 @@ CREATE TABLE CastMembers(
 	CastMemberID INT PRIMARY KEY AUTO_INCREMENT,
     ActorID INT NOT NULL,
     MovieID INT NOT NULL,
-    Role VARCHAR(50) NOT NULL,
+    ActorRole VARCHAR(50) NOT NULL,
     FOREIGN KEY fk_CastMembers_Actor (ActorID)
 		REFERENCES Actor(ActorID),
 	FOREIGN KEY fk_CastMembers_Movie (MovieID)
 		REFERENCES Movie(MovieID)
 );
+
+
+-- DML Exercise breakline
+
+INSERT INTO Actor(FirstName, LastName, BirthDate) VALUES
+	('Bill', 'Murray', '1950-09-21'),
+    ('Dan', 'Akroyd', '1952-07-01'),
+    ('John', 'Candy', '1950-10-31'),
+    ('Steve', 'Martin', NULL),
+    ('Sylvester', 'Stallone', NULL);
+        
+INSERT INTO Director(FirstName, LastName, BirthDate) VALUES
+	('Ivan', 'Reltman', '1946-10-27'),
+    ('Ted', 'Kotcheff', NULL);
+
+INSERT INTO Rating(RatingName) VALUES
+	('G'), ('PG'), ('PG-13'), ('R');
+
+INSERT INTO Genre(GenreName) VALUES
+	('Action'), ('Comedy'), ('Drama'), ('Horror');
+
+INSERT INTO Movie(MovieGenreId, MovieDirectorId, MovieRatingId, Title, ReleaseDate) VALUES
+	(1, 2, 4, 'Rambo: First Blood', '1982-10-22'),
+    (2, NULL,  4, 'Planes, Trains & Automobiles', '1987-11-25'),
+    (2, 1, 2, 'Ghostbusters', NULL),
+    (2, NULL, 2, 'The Great Outdoors', '1988-06-17');
+
+INSERT INTO CastMembers(ActorID, MovieID, ActorRole) VALUES
+	(5, 1, 'John Rambo'),
+    (4, 2, 'Neil Page'),
+    (3, 2, 'Del Griffith'),
+    (1, 3, 'Dr. Peter Venkman'),
+    (2, 3, 'Dr. Raymond Stanz'),
+    (2, 4, 'Roman Craig'),
+    (3, 4, 'Chet Ripley');
