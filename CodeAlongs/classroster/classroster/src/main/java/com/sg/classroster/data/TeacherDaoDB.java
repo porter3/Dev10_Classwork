0b6ad3c769066667864cfd428202f037bb1d439b
porter3 <45975762+porter3@users.noreply.github.com>
2020-01-27 19:06:35 -0500

    created all dao interfaces/classes/methods

--- CodeAlongs/classroster/classroster/src/main/java/com/sg/classroster/data/TeacherDaoDB.java
+++ CodeAlongs/classroster/classroster/src/main/java/com/sg/classroster/data/TeacherDaoDB.java
@@ -14,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
  *
  * @author jake
  */
-public class TeachDaoDB implements TeacherDao{
+public class TeacherDaoDB implements TeacherDao{
     
     @Autowired
     JdbcTemplate jdbc;
--- CodeAlongs/classroster/classroster/src/main/java/com/sg/classroster/entities/Course.java
+++ CodeAlongs/classroster/classroster/src/main/java/com/sg/classroster/entities/Course.java
@@ -1,6 +1,7 @@
 package com.sg.classroster.entities;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  *
@@ -16,4 +17,86 @@ public class Course {
     private String description;
     private Teacher teacher;
     private List<Student> students;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + this.id;
+        hash = 59 * hash + Objects.hashCode(this.name);
+        hash = 59 * hash + Objects.hashCode(this.description);
+        hash = 59 * hash + Objects.hashCode(this.teacher);
+        hash = 59 * hash + Objects.hashCode(this.students);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Course other = (Course) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.teacher, other.teacher)) {
+            return false;
+        }
+        if (!Objects.equals(this.students, other.students)) {
+            return false;
+        }
+        return true;
+    }
+    
 }
