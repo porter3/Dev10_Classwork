13822c49fb24b99a0e5afd0dccc987c39ac94635
porter3 <45975762+porter3@users.noreply.github.com>
2020-01-30 14:53:20 -0500

    adding exercise to repo

---
+++ CodeAlongs/inventorystarter/inventorystarter/src/main/java/com/sg/inventory/entities/Store.java
@@ -0,0 +1,50 @@
+package com.sg.inventory.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Store {
+
+    private int id;
+    
+    private String name;
+    
+    private String location;
+    
+    private String manager;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getManager() {
+        return manager;
+    }
+
+    public void setManager(String manager) {
+        this.manager = manager;
+    }
+    
+    
+    
+}
